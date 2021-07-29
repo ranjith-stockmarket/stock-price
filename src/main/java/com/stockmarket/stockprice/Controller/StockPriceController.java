4ea7a2a2ea984c30c0b23feaee229c404e782323
Ranjith Thangaraju <ranjithshuttler10@gmail.com>
2021-07-29 10:17:01 +0530

    Final

--- src/main/java/com/stockmarket/stockprice/Controller/StockPriceController.java
+++ src/main/java/com/stockmarket/stockprice/Controller/StockPriceController.java
@@ -3,10 +3,7 @@ package com.stockmarket.stockprice.Controller;
 import com.stockmarket.stockprice.DTO.AddIpoStockExchangeDTO;
 import com.stockmarket.stockprice.Service.StockPriceService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/stock-price")
